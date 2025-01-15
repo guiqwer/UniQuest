37064c6f3d7e5cc1fc3eace426abe82ff02954db
guiqwer <guilherme.souza0996@gmail.com>
2025-01-15 15:22:43 -0300

    entity user

---
+++ src/main/java/com/Uniquest/UniQuest/UniQuestApplication.java
@@ -0,0 +1,13 @@
+package com.Uniquest.UniQuest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniQuestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniQuestApplication.class, args);
+	}
+
+}
