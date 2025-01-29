dbab811b52a9aa4682b5da0fc4c7087ea9ffcd98
guiqwer <guilherme.souza0996@gmail.com>
2025-01-29 19:16:32 -0300

    ref(controllers) realocando endpoint register para UserController

--- backend/src/main/java/com/Uniquest/UniQuest/controllers/UserController.java
+++ backend/src/main/java/com/Uniquest/UniQuest/controllers/UserController.java
@@ -2,17 +2,24 @@ package com.Uniquest.UniQuest.controllers;
 
 
 import com.Uniquest.UniQuest.domain.user.User;
+import com.Uniquest.UniQuest.dto.RegisterRequestDTO;
+import com.Uniquest.UniQuest.dto.ResponseDTO;
+import com.Uniquest.UniQuest.infra.security.TokenService;
 import com.Uniquest.UniQuest.repositories.UserRepository;
 import com.Uniquest.UniQuest.service.EmailService;
 import com.Uniquest.UniQuest.service.UserService;
 import com.Uniquest.UniQuest.utils.GenericResponse;
 import com.Uniquest.UniQuest.utils.UrlUtil;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 
+import java.util.Optional;
 import java.util.UUID;
 
 import static com.Uniquest.UniQuest.utils.UrlUtil.getAppUrl;
@@ -25,10 +32,20 @@ public class UserController {
     private final EmailService emailService;
     private GenericResponse message;
 
-    public UserController(UserService userService, JavaMailSenderImpl mailSender, EmailService emailService) {
+    @Autowired
+    private final UserRepository repository;
+    @Autowired
+    private final PasswordEncoder passwordEncoder;
+    @Autowired
+    private final TokenService tokenService;
+
+    public UserController(UserService userService, JavaMailSenderImpl mailSender, EmailService emailService, UserRepository repository, PasswordEncoder passwordEncoder, TokenService tokenService) {
         this.userService = userService;
         this.mailSender = mailSender;
         this.emailService = emailService;
+        this.repository = repository;
+        this.passwordEncoder = passwordEncoder;
+        this.tokenService = tokenService;
     }
 
     @PostMapping("/resetPassword")
@@ -45,6 +62,23 @@ public class UserController {
         mailSender.send(emailService.constructResetTokenEmail(getAppUrl(request), request.getLocale(), token, user));
 
         return new GenericResponse(message.getMessage());
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity register(@RequestBody RegisterRequestDTO body){
+        Optional<User> user = this.repository.findByEmail(body.email());
+
+        if(user.isEmpty()) {
+            User newUser = new User();
+            newUser.setPassword(passwordEncoder.encode(body.password()));
+            newUser.setEmail(body.email());
+            newUser.setName(body.name());
+            this.repository.save(newUser);
+
+            String token = this.tokenService.generateToken(newUser);
+            return ResponseEntity.ok(new ResponseDTO(newUser.getName(), token));
+        }
+        return ResponseEntity.badRequest().build();
     }
 
 
