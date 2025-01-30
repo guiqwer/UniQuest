49f2522a59a0624b52494f94ae6aaa32b8f102b8
guiqwer <guilherme.souza0996@gmail.com>
2025-01-30 13:42:25 -0300

    ref(User) tirando atributos nickname/username

--- backend/src/main/java/com/Uniquest/UniQuest/domain/user/User.java
+++ backend/src/main/java/com/Uniquest/UniQuest/domain/user/User.java
@@ -1,12 +1,10 @@
 package com.Uniquest.UniQuest.domain.user;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
+@Data
 @Table(name = "users")
 @Getter
 @Setter
