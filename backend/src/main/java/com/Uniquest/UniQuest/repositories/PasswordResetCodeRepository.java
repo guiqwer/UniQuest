f8b934e26497a1cd2818f0d8da6cc5d6d0c18c23
guiqwer <guilherme.souza0996@gmail.com>
2025-02-02 12:39:44 -0300

    feat: repository pra achar o resetcode

--- backend/src/main/java/com/Uniquest/UniQuest/repositories/PasswordResetCodeRepository.java
+++ backend/src/main/java/com/Uniquest/UniQuest/repositories/PasswordResetCodeRepository.java
@@ -1,7 +1,6 @@
 package com.Uniquest.UniQuest.repositories;
 
 import com.Uniquest.UniQuest.domain.user.PasswordResetCode;
-import com.Uniquest.UniQuest.domain.user.PasswordResetToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
