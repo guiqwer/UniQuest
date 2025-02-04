7c42a83764fd6b656f5718c44a070ec3c727fec9
Lucas-Delacroix <delacroixdev@gmail.com>
2025-02-03 22:45:34 -0300

    ref: Atualiza o domain de ObjectiveQuestion

--- backend/src/main/java/com/Uniquest/UniQuest/domain/question/ObjectiveQuestion.java
+++ backend/src/main/java/com/Uniquest/UniQuest/domain/question/ObjectiveQuestion.java
@@ -1,11 +1,13 @@
 package com.Uniquest.UniQuest.domain.question;
 
+import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import lombok.*;
 
 import java.util.List;
 
 @Entity
+@DiscriminatorValue("OBJECTIVE")
 @Getter
 @Setter
 public class ObjectiveQuestion extends Question {
