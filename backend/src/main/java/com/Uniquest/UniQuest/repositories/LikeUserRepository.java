0964935d9c4479bcbe1182b59bedcb03c87c9be1
guiqwer <guilherme.souza0996@gmail.com>
2025-02-04 17:05:21 -0300

    

--- backend/src/main/java/com/Uniquest/UniQuest/repositories/LikeUserRepository.java
+++ backend/src/main/java/com/Uniquest/UniQuest/repositories/LikeUserRepository.java
@@ -1,12 +1,12 @@
 package com.Uniquest.UniQuest.repositories;
 
 import com.Uniquest.UniQuest.domain.exam.Exam;
-import com.Uniquest.UniQuest.domain.user.InteractionUser;
+import com.Uniquest.UniQuest.domain.user.LikeUser;
 import com.Uniquest.UniQuest.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface InteractionUserRepository extends JpaRepository<InteractionUser, Long> {
-    Optional<InteractionUser> findByUserAndExam(User user, Exam exam); // Verifica se o usuário já interagiu com a prova
+public interface LikeUserRepository extends JpaRepository<LikeUser, Long> {
+    Optional<LikeUser> findByUserAndExam(User user, Exam exam); // Verifica se o usuário já interagiu com a prova
 }
