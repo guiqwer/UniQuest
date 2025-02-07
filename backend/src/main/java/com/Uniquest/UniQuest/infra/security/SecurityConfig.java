b06684bbbf6b912a8ff91067ec64ee98ee7e4726
guiqwer <guilherme.souza0996@gmail.com>
2025-02-07 14:52:52 -0300

    test

--- backend/src/main/java/com/Uniquest/UniQuest/infra/security/SecurityConfig.java
+++ backend/src/main/java/com/Uniquest/UniQuest/infra/security/SecurityConfig.java
@@ -16,7 +16,6 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import java.util.List;
 
@@ -25,7 +24,7 @@ import java.util.List;
 public class SecurityConfig {
 
     @Autowired
-    private CustomDetailsService userDetailsService;
+    private CustomUserDetailsService userDetailsService;
 
     @Autowired
     SecurityFilter securityFilter;
